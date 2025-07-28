7c839e5f2a34a2670f2d912a7e08014cb034c5dd
Jorge Schiavon <105507250+jlschiavon@users.noreply.github.com>
2025-07-27 20:41:15 -0600

    Update chat_analysis_app.py

--- chat_analysis_app.py
+++ chat_analysis_app.py
@@ -157,6 +157,29 @@ def detectar_respuestas(datos):
 
     return respuestas
 
+def carga_ALDS(df):
+    df = pd.read_csv(r"C:\Users\jorge\OneDrive - Instituto Tecnologico y de Estudios Superiores de Monterrey\Desktop\streamlit_reporte_ALDS\05 - Overview (Parts worked in stations per shift).csv", skiprows=14)
+    df.dropna(how="all", axis=1, inplace=True)
+    df.dropna(how="all", axis=0, inplace=True)
+    df = df.loc[:, ~df.columns.str.contains("Unnamed")]
+    df["Station"] = df["Station"].where(df["Station"].str.startswith("Reckstation", na=False)).ffill()
+
+    partes = orden_partes
+    ALDS = []
+
+    for shift in shifts:
+        df_shift = df[df["Shift"] == shift]
+        for parte in partes:
+            if parte not in df.columns:
+                continue
+            filtro = df_shift[parte] != 0
+            total_serie = df_shift.loc[filtro, "Serie Parts"].sum() if filtro.any() else 0
+            total_rework = df_shift.loc[filtro, "Rework Parts"].sum() if filtro.any() else 0
+            ALDS.append({"Shift": shift, "Parte": parte, "ALDS Serie": total_serie, "ALDS Rework": total_rework})
+
+    df_resultados = pd.DataFrame(ALDS).set_index(["Shift", "Parte"]).reindex(index_completo, fill_value=0).reset_index()
+
+
 # ------------------ INTERFAZ ----------------------
 from PIL import Image
 import os
