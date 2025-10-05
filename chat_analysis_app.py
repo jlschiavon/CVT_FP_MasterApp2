7877a975bcd10ad670f51e7ce24b20b9a5b2223b
Jorge Schiavon <105507250+jlschiavon@users.noreply.github.com>
2025-10-04 22:38:10 -0600

    Update chat_analysis_app.py

--- chat_analysis_app.py
+++ chat_analysis_app.py
@@ -222,7 +222,7 @@ if len(oee_dict) > 0:
         color = (
             "green" 
             if (("Recken" in machine and (target_recken - 5 <= val <= target_recken + 5)) 
-            or ("VPK" in machine and (target_vpk - 5 <= val <= target_vpk + 5))) 
+                or ("VPK" in machine and (target_vpk - 5 <= val <= target_vpk + 5))) 
             else "red"
         )
 
